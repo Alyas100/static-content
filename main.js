78b41ee7c9eee8d865c21b6e22e02acd886a3a83
Alyas <alyasmuhd1234@gmail.com>
2025-04-18 19:35:43 +0800

    fixing request to backend

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 // to fetch comments
-fetch("http://localhost/api/comments")
+fetch("https://blogmine1234.netlify.app/")
   .then(res => res.json())
   .then(data => {
     document.getElementById("comments").innerHTML = data.map(c => `<p>${c.text}</p>`).join("");
